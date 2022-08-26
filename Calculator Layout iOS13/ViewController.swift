02104538a956c636bf84e769b7d921cb1b5976f9
Enzo Rodrigues Soares <enzors@MMB027836BHZ.local>
2022-08-26 13:16:49 -0300

    Adding functions to buttons

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -10,11 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var topLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        bottomLabel.text! += sender.currentTitle!
+    }
+    
+    @IBAction func operationButtomPressed(_ sender: UIButton) {
+        topLabel.text! += "\(bottomLabel.text!) \(sender.currentTitle!) "
+        
+        bottomLabel.text! = ""
+    }
+    
+    @IBAction func equalsButtonPressed(_ sender: Any) {
+        let topEquation = topLabel.text!
+        let bottomValues = bottomLabel.text!
+        let equation = topEquation + bottomValues
 
+        topLabel.text! = ""
+        
+        let result = NSExpression(format: equation)
+        
+        let v = result.expressionValue(with: nil, context: nil) ?? "Error!"
+        
+        bottomLabel.text! = "\(v)"
+        
+        
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        
+        bottomLabel.text! = ""
+        
+    }
+    
+    @IBAction func clearButtonDoubleTouch(_ sender: Any) {
+        topLabel.text! = ""
+    }
+    
+    
+    @IBAction func negateButtonPressed(_ sender: Any) {
+        var value = bottomLabel.text!
+        if value.contains("-"){
+            value.removeFirst()
+        }
+        else {
+            value = "-\(value)"
+        }
+        
+        bottomLabel.text! = value
+    }
+    @IBAction func perCentButtonPressed(_ sender: Any) {
+        let percent = "\(bottomLabel.text!)/100.0"
+        let result = NSExpression(format: percent)
+        
+        let v = result.expressionValue(with: nil, context: nil) ?? "Error!"
+        print(v)
+        
+        bottomLabel.text! = "\(v)"
+    }
 }
 
